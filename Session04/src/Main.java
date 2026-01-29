67d4a65cee7127b903b0b34f544f870ff5789cb5
dangtoan <dangtoan.2k6@gmail.com>
2026-01-29 10:58:01 +0700

    ss4

---
+++ Session04/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//          Bai01.formatEx();
+//
+////          Bai02.execute();
+//            Bai03.execute();
+//        Bai04.execute(sc);
+        Bai05.execute();
+    }
+//    }
+}
